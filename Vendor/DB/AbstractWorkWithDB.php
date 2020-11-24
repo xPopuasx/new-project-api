ee4d37a1ec2efb93e8ec14c53233ff728d00612e
xPopuasx <strong.rahs@gmail.com>
2020-11-24 23:59:11 +0500

    change many files

--- Vendor/DB/AbstractWorkWithDB.php
+++ Vendor/DB/AbstractWorkWithDB.php
@@ -2,8 +2,39 @@
 
 namespace DB;
 
+use DB\MysqlConnect;
 
 abstract class AbstractWorkWithDB implements WorkWithDBInterface{
+
+    private $connect = false
+
+    public function select($query)
+    {
+        $this->connect = new MysqlConnect();
+        try{
+            if($this->connect->getInstanc === true)
+            {
+
+            }
+            else
+            {
+                trow new \Exception('Ошибка подключения');
+            }
+        }
+        catch (Exception $e)
+
+
+    }
+
+    public function insert($query)
+    {
+        // TODO: Implement insert() method.
+    }
+
+    public function join($query)
+    {
+        // TODO: Implement join() method.
+    }
 
 
 }
--- Vendor/DB/MysqlConnect.php
+++ Vendor/DB/MysqlConnect.php
@@ -8,19 +8,31 @@ class MysqlConnect
 {
     public static $connect = false;
 
+    /*
+     * @return bool
+     */
     public function __construct()
     {
         $connect_params = require_once $_SERVER['DOCUMENT_ROOT'].'/config.php';
-        print_r($connect_params);
 
         self::$connect = mysqli_connect(
             $connect_params['DB_CONNECT']['locahost'],
             $connect_params['DB_CONNECT']['user'],
             $connect_params['DB_CONNECT']['password'],
-            $connect_params['DB_CONNECT']['db_name']);
+            $connect_params['DB_CONNECT']['db_test']);
+    }
 
+    /*
+     * Одиночка
+     * @return bool
+    */
+    public static function getInstance()
+    {
+        if (self::$connect === true) {
+            return self::$connect;
+        }
 
-
+        return new self;
     }
 
 }
